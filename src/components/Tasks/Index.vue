3f956ee2c9f35cd776d0dc9159726e93edd2fb74
yusuke ohno <yusuke@ohno.systems>
2019-09-03 00:36:52 +0900

    update

--- src/components/Tasks/Index.vue
+++ src/components/Tasks/Index.vue
@@ -2,20 +2,45 @@
   <div>
     <h2>タスク一覧</h2>
     <ul>
-      <li v-for="task in tasks" v-bind:key="task.id">
-        <input type="checkbox" v-bind:checked="task.done" />
+      <li v-for="task in tasks" :key="task.id">
+        <input
+          type="checkbox"
+          :checked="task.done"
+          @change="toggleTaskStatus(task)"
+        />
         {{ task.name }}
       </li>
     </ul>
+    <form @submit.prevent="addTask">
+      <input type="text" v-model="newTaskName" placeholder="新しいタスク" />
+    </form>
   </div>
 </template>
 
 <script>
 export default {
   name: "Tasks",
+  data() {
+    return {
+      newTaskName: ""
+    };
+  },
   computed: {
     tasks() {
       return this.$store.state.tasks;
+    }
+  },
+  methods: {
+    addTask() {
+      this.$store.commit("addTask", {
+        name: this.newTaskName
+      });
+      this.newTaskName = "";
+    },
+    toggleTaskStatus(task) {
+      this.$store.commit("toggleTaskStatus", {
+        id: task.id
+      });
     }
   }
 };
--- src/stores/index.js
+++ src/stores/index.js
@@ -16,7 +16,27 @@ const store = new Vuex.Store({
         name: "Vue.jsの本を買う",
         done: true
       }
-    ]
+    ],
+    nextTaskId: 3
+  },
+  mutations: {
+    addTask(state, { name }) {
+      state.tasks.push({
+        id: state.nextTaskId,
+        name,
+        done: false
+      });
+      state.nextTaskId++;
+    },
+    toggleTaskStatus(state, { id }) {
+      const filtered = state.tasks.filter(task => {
+        return task.id === id;
+      });
+
+      filtered.forEach(task => {
+        task.done = !task.done;
+      });
+    }
   }
 });
 export default store;
